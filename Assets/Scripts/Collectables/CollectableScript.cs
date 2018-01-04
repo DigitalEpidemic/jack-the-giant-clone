6d5fd1115af49a6e31b5923224159ab36ed144d0
Jeff Polasz <jeff_polasz@hotmail.com>
2018-01-04 17:34:24 -0500

    Fixed collectables disappearing too early

--- Assets/Scripts/Collectables/CollectableScript.cs
+++ Assets/Scripts/Collectables/CollectableScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CollectableScript : MonoBehaviour {
 
 	void OnEnable () {
-		Invoke ("DestroyCollectable", 6f);
+		Invoke ("DestroyCollectable", 7f);
 	}
 
 	void DestroyCollectable () {
